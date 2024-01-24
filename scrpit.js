c32fea29695c22c715da19b4f6b3227362fc07da
Gabrielt3123 <132002025+Gabrielt312@users.noreply.github.com>
2024-01-24 17:09:01 -0300

    Add files via upload

---
+++ scrpit.js
@@ -0,0 +1,23 @@
+window.sr = ScrollReveal({ reset: true });
+
+sr.reveal('.container', { delay: 100 });
+
+sr.reveal('.container-tree', { 
+    rotate: { x: 0, y: 80, z: 0,
+    duration: 6000}
+ });
+
+ sr.reveal('.container-four', { 
+    rotate: { x: 0, y: 80, z: 0,
+    duration: 6000}
+ });
+ 
+ sr.reveal('.container-five', { 
+    rotate: { x: 0, y: 80, z: 0,
+    duration: 6000}
+ });
+
+ sr.reveal('.container-six', { 
+    rotate: { x: 0, y: 80, z: 0,
+    duration: 6000}
+ });
